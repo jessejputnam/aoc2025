05d964149d558ea1504576b217f9eb85dae82ca9
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2025-12-16 21:46:32 -0500

    Complete day 5

--- lib.py
+++ lib.py
@@ -8,39 +8,65 @@ load_dotenv()
 session_id = os.getenv("SESSION_ID", "")
 
 
-def clean_data(data: str, splitVal: str) -> list[str]:
+def clean_data(data: str, split_val: str) -> list[str]:
     """Cleans the input data by splitting it using the specified delimiter
     and stripping whitespace from each element."""
     try:
-        return [x.strip() for x in data.split(splitVal) if len(x.strip()) > 0]
+        return [x.strip() for x in data.split(split_val) if len(x.strip()) > 0]
     except Exception as e:
         print(f"Error cleaning data: {e}")
         raise
 
 
-def get_data(day: int, splitVal: str) -> list[str]:
-    """Fetches the input data for the specified day from the Advent of Code website
-    using the session ID for authentication."""
+def clean_data_multipart(data: str, split_val: str) -> list[list[str]]:
+    """Cleans the input data by splitting it using the specified delimiter
+    and stripping whitespace from each element where directions are divided."""
+    try:
+        top, bottom = data.split("\n\n")
+        return [
+            [x.strip() for x in top.split(split_val) if len(x.strip()) > 0],
+            [x.strip() for x in bottom.split(split_val) if len(x.strip()) > 0],
+        ]
+    except Exception as e:
+        print(f"Error cleaning data: {e}")
+        raise
+
+
+def get_data(day: int, split_val: str, type: str = "full") -> list[str]:
+    """Fetches the input data for the specified day"""
     url = f"https://adventofcode.com/2025/day/{day}/input"
     cookie = {"session": session_id}
 
     try:
-        response = requests.get(url, cookies=cookie)
-        return clean_data(response.text, splitVal)
+        if type == "test":
+            with open("test_data/db.json", "r") as file:
+                data = json.load(file)
+                key = f"day{str(day).zfill(2)}"
+                return clean_data(data[key], split_val)
+        else:
+            response = requests.get(url, cookies=cookie)
+            return clean_data(response.text, split_val)
     except Exception as e:
-        print(f"Error fetching test data: {e}")
+        print(f"Error fetching data: {e}")
         raise
 
 
-def get_test_data(day: int, splitVal: str) -> list[str]:
-    """Fetches the test data for the specified day from a local JSON file."""
+def get_data_multipart(day: int, split_val: str, type: str = "full") -> list[list[str]]:
+    """Fetches the data for the specified day where directions are divided."""
+    url = f"https://adventofcode.com/2025/day/{day}/input"
+    cookie = {"session": session_id}
+
     try:
-        with open("test_data/db.json", "r") as file:
-            data = json.load(file)
-            key = f"day{str(day).zfill(2)}"
-            return clean_data(data[key], splitVal)
+        if type == "test":
+            with open("test_data/db.json", "r") as file:
+                data = json.load(file)
+                key = f"day{str(day).zfill(2)}"
+                return clean_data_multipart(data[key], split_val)
+        else:
+            response = requests.get(url, cookies=cookie)
+            return clean_data_multipart(response.text, split_val)
     except Exception as e:
-        print(f"Error fetching test data: {e}")
+        print(f"Error fetching data: {e}")
         raise
 
 
